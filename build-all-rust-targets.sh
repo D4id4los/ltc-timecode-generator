c15c72d59bc1177b0e40eb520b4807677ee9781e
Viktoria Pogrzebacz <viktoria@agerekg.at>
2026-08-07 13:54:34 +0200

    Add a build script that will build all supported rust targets.

---
+++ build-all-rust-targets.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Building linux x64 release..."
+cargo zigbuild --release
+echo "Building windows x64 release..."
+cargo zigbuild --target x86_64-pc-windows-gnu --release
+echo "Building linux x32 release..."
+docker run --rm \
+         -v "$(pwd)":/app \
+         -v "$(pwd)/.cargo_cache:/usr/local/cargo/registry" \
+         -w /app/ltc-gui \
+         ltc-debian11-builder \
+         cargo zigbuild --target i686-unknown-linux-gnu.2.31 --release
